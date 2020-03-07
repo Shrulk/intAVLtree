b78e581f4f612aa0765e356b9cf0d2a54221b81e
Shrulk <58909805+Shrulk@users.noreply.github.com>
2020-03-07 12:50:58 +0300

    Изменёна реализация интерфейса

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ template <class C> class Container
 {
 public:
     // Виртуальные методы, должны быть реализованы вашим контейнером
-    virtual void Insert(int value) = 0;
-    virtual bool Exists(int value) = 0;
-    virtual void Remove(int value) = 0;
+    virtual void insert(int value) = 0;
+    virtual bool exists(int value) = 0;
+    virtual void remove(int value) = 0;
 
     // И этот тоже, хотя к нему потом ещё вернёмся
-    virtual void ViewLevels() = 0;
+    virtual void print() = 0;
 
-    virtual void Display() = 0;
+    //virtual void Display() = 0;
 
     // Виртуальный деструктор (пока просто поверьте, что он нужен)
     virtual ~Container() { };
@@ -33,7 +33,7 @@ private:
 public:
     template <class T> friend class AVLTree;
     Node() : data(0), l(NULL), r(NULL), h(0) {}
-    Node (const A i =0) : data(i), l(NULL), r(NULL), h(0) {}
+    Node (const A i =0) : data(i), l(nullptr), r(nullptr), h(0) {}
     Node (const Node & x): data(x.data), l(x.l), r(x.r), h(x.h) {}
     ~Node()
     {
@@ -46,14 +46,14 @@ public:
 template<class T> class AVLTree: public Container<class C>
 {
 public:
-    AVLTree() : root(NULL) {}
-    void Remove(T value){
+    AVLTree() : root(nullptr) {}
+    void remove(T value){
         _remove(root, value);
     }
-    bool Exists(T key){
+    bool exists(T key){
         _exists(root, key);
     }
-    void Insert(const  T key)
+    void insert(const  T key)
     {
         root = _insert(root, key);
     }
@@ -68,7 +68,7 @@ public:
         _delete(root);
     }
 
-    void ViewLevels()
+    void print() override
     {
         for (int i = 0; i < _height(root); i++)
         {
@@ -88,7 +88,7 @@ private:
 
     /*void _remove(const Node <T> * pNode, T key){
         Node <T> * tmp = _find(pNode, key);
-        if(!tmp->right && !tmp->left)
+        if(!tmp -> right && !tmp -> left)
             delete pNode;
         if()
     }*/
@@ -101,45 +101,45 @@ private:
     int _height(const Node <T> * pNode)
     {
         //t++;
-        return pNode ? pNode->h : 0;
+        return pNode ? pNode -> h : 0;
     }
 
     int _balance(const Node <T> * pNode)
     {
-        return pNode ? (pNode->l? pNode->l->h:0) - (pNode->r? pNode->r->h:0) : 0;
+        return pNode ? (pNode -> l? pNode -> l -> h:0) - (pNode -> r? pNode -> r -> h:0) : 0;
     }
 
     Node <T> * _l_rotation(Node <T> * pNode)
     {
-        Node <T> * tmpNode = pNode->r;
-        pNode->r = tmpNode->l;
-        tmpNode->l = pNode;
-        pNode->h = max(_height(pNode->l),_height(pNode->r))+1;
-        tmpNode->h = max(_height(tmpNode->l),_height(tmpNode->r))+1;
+        Node <T> * tmpNode = pNode -> r;
+        pNode -> r = tmpNode -> l;
+        tmpNode -> l = pNode;
+        pNode -> h = max(_height(pNode -> l),_height(pNode -> r))+1;
+        tmpNode -> h = max(_height(tmpNode -> l),_height(tmpNode -> r))+1;
         return tmpNode;
     }
 
     Node <T> * _r_rotation(Node <T> * pNode)
     {
-        Node <T> * tmpNode = pNode->l;
-        pNode->l = tmpNode->r;
-        tmpNode->r = pNode;
-        pNode->h = max(_height(pNode->l),_height(pNode->r))+1;
-        tmpNode->h = max(_height(tmpNode->l),_height(tmpNode->r))+1;
+        Node <T> * tmpNode = pNode -> l;
+        pNode -> l = tmpNode -> r;
+        tmpNode -> r = pNode;
+        pNode -> h = max(_height(pNode -> l),_height(pNode -> r))+1;
+        tmpNode -> h = max(_height(tmpNode -> l),_height(tmpNode -> r))+1;
         return tmpNode;
     }
 
     Node <T> * _R_rotation(Node <T> * pNode)
     {
-        Node <T> * tmpNode = pNode->l;
-        pNode->l = _l_rotation(tmpNode);
+        Node <T> * tmpNode = pNode -> l;
+        pNode -> l = _l_rotation(tmpNode);
         return _r_rotation(pNode);
     }
 
     Node <T> * _L_rotation(Node <T> * pNode)
     {
-        Node <T> * tmpNode = pNode->r;
-        pNode->r = _r_rotation(tmpNode);
+        Node <T> * tmpNode = pNode -> r;
+        pNode -> r = _r_rotation(tmpNode);
         return _l_rotation(pNode);
     }
 
@@ -148,14 +148,14 @@ private:
         int b_factor = _balance(pNode);
         if (b_factor > 1)
         {
-            if (_balance(pNode->l) >= 0) pNode = _r_rotation(pNode);
+            if (_balance(pNode -> l) >= 0) pNode = _r_rotation(pNode);
             else pNode = _R_rotation(pNode);
         }
         else
         {
             if (b_factor < -1)
             {
-                if (_balance(pNode->r) <= 0) pNode = _l_rotation(pNode);
+                if (_balance(pNode -> r) <= 0) pNode = _l_rotation(pNode);
                 else pNode = _L_rotation(pNode);
             }
         }
@@ -165,46 +165,46 @@ private:
     Node <T> * _insert(Node <T> * pNode, const  T key)
     {
         if (!pNode) return new Node<T>(key);
-        else if (key < pNode->data)
+        else if (key < pNode -> data)
         {
-            pNode->l = _insert(pNode->l, key);
+            pNode -> l = _insert(pNode -> l, key);
             pNode = _balancing(pNode);
         }
-        else if (key > pNode->data)
+        else if (key > pNode -> data)
         {
-            pNode->r = _insert(pNode->r, key);
+            pNode -> r = _insert(pNode -> r, key);
             pNode = _balancing(pNode);
         }
         else
         {
             //Already existed
-            return 0;
+            return nullptr;
         }
-        pNode->h = max(_height(pNode->l),_height(pNode->r))+1;
+        pNode -> h = max(_height(pNode -> l),_height(pNode -> r))+1;
         return pNode;
     }
 
     void _display(Node <T> * pNode)
     {
         if (!pNode) return;
-        _display(pNode->l);
-        cout << pNode->data << ' ';
-        _display(pNode->r);
+        _display(pNode -> l);
+        cout << pNode -> data << ' ';
+        _display(pNode -> r);
     }
 
     /*	void _delete(Node <T> * pNode)
     {
         if (!pNode) return;
-        if(pNode->l != NULL)
+        if(pNode -> l != NULL)
         {
-            _delete(pNode->l);
+            _delete(pNode -> l);
         }
-        if(pNode->r != NULL)
+        if(pNode -> r != NULL)
         {
-            _delete(pNode->r);
+            _delete(pNode -> r);
         }
         delete pNode;
-        //_remove(pNode, pNode->data);
+        //_remove(pNode, pNode -> data);
     }*/
 
     void _delete(Node <T> * pNode)
@@ -225,61 +225,60 @@ private:
 
     Node <T> * _find_min( Node < T> * pNode)
     {
-        return pNode->l ? _find_min(pNode->l) : pNode;
+        return pNode -> l ? _find_min(pNode -> l) : pNode;
     }
 
     Node <T> * _find_max(const Node < T> * pNode)
     {
-        return pNode->r ? _find_max(pNode->r) : pNode;
+
+        return pNode -> r ? _find_max(pNode -> r) : pNode;
     }
 
     Node <T> * _find(Node <T> * pNode, const  T key)
     {
-        if (pNode == NULL) return NULL;
-        else if (pNode->data == key) return pNode;
-        else return pNode->data > key ? _find(pNode->l, key) : _find(pNode->r, key);
+        if (pNode == nullptr) return nullptr;
+        else if (pNode -> data == key) return pNode;
+        else return pNode -> data > key ? _find(pNode -> l, key) : _find(pNode -> r, key);
     }
-
     void _remove(Node <T> * pNode, const  T key)
     {
         Node<T> * tmpNode = _find(pNode, key);
-        if (tmpNode == NULL) return; //pNode;
-        else
-        {
-            if (tmpNode->l && tmpNode->r)
+        if (tmpNode == nullptr) return; //pNode;
+            if (tmpNode -> l && tmpNode -> r)
             {
                 //Left and right subtree
                 Node<T> * maxTmpNode = _find_min(tmpNode);
-                tmpNode->data = maxTmpNode->data;
+                tmpNode -> data = maxTmpNode -> data;
+
                 delete maxTmpNode;
             }
             else
             {
-                if (tmpNode->l)
+                if (tmpNode -> l)
                 {
                     //only left subtree
-                    Node<T> * leftTmpNode = tmpNode->l;
+                    Node<T> * leftTmpNode = tmpNode -> l;
                     tmpNode->data = leftTmpNode->data;
-                    tmpNode->l = leftTmpNode->l;
-                    tmpNode->r = leftTmpNode->r;
+                    tmpNode -> l = leftTmpNode -> l;
+                    tmpNode -> r = leftTmpNode -> r;
                     delete leftTmpNode;
 
                 }
                 else
                 {
-                    if (tmpNode->r)
+                    if (tmpNode -> r)
                     {
                         //only right subtree
-                        Node<T> * rightTmpNode = tmpNode->r;
+                        Node<T> * rightTmpNode = tmpNode -> r;
                         tmpNode->data = rightTmpNode->data;
-                        tmpNode->l = rightTmpNode->l;
-                        tmpNode->r = rightTmpNode->r;
+                        tmpNode -> l = rightTmpNode -> l;
+                        tmpNode -> r = rightTmpNode -> r;
                         delete rightTmpNode;
                     }
                     else
                     {
-                        tmpNode->l = NULL;
-                        tmpNode->r = NULL;
+                        tmpNode -> l = nullptr;
+                        tmpNode -> r = nullptr;
                         //no subtree
                         delete tmpNode;
                     }
@@ -288,15 +287,14 @@ private:
                 }
             }
         }
-    }
 
     void viewTreeLevel(int i, const Node<T> * pNode)
     {
         if (i == 0) cout << pNode->data << ' ';
         else
         {
-            if (pNode->l) viewTreeLevel(i - 1, pNode->l);
-            if (pNode->r) viewTreeLevel(i - 1, pNode->r);
+            if (pNode -> l) viewTreeLevel(i - 1, pNode -> l);
+            if (pNode -> r) viewTreeLevel(i - 1, pNode -> r);
         }
     }
 };
@@ -322,22 +320,22 @@ int main()
     Container<int>* c = reinterpret_cast<Container<int> *>(new AVLTree<int>());
 
     for(int i = 1; i < 10; i++)
-        c->Insert(i*i);
+        c->insert(i*i);
 
     cout << "Container after creation:" << endl;
-    c->ViewLevels();
+    c->print();
 
-    if(c->Exists(25))
+    if(c->exists(25))
         cout << "Search for value 25: found" << endl;
 
-    if(!c->Exists(111))
+    if(!c->exists(111))
         cout << "Search for value 111: not found" << endl;
 
     //c->Display();
 
-    c->Remove(36);
+    c -> remove(36);
     cout << "Container after deletion of the element:" << endl;
-    c->ViewLevels();
+    c->print();
 
     delete c;
     return 0;
